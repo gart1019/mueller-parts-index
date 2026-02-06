a0f9899b28c2f3f026a65494c01a40478d04fdda
Garrett <gnorman376@gmail.com>
2026-02-06 01:30:36 -0600

    Add login fields + route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
 from flask import render_template
+from app.forms import LoginForm
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+@app.route('/lookup')
+def lookup():
+    return render_template('lookup.html')
