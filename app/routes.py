0a2044bca3bd1cbf868e614fb122daf4b619e9a7
Garrett <gnorman376@gmail.com>
2026-02-06 13:15:53 -0600

    Pre-db additions

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
-from app.forms import LoginForm
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 import os
 
+from dotenv import find_dotenv, load_dotenv
+
+path = find_dotenv()
+load_dotenv(path)
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'bad key'
     SQLALCHEMY_ENGINES = {"default": "sqlite:///default.sqlite"}
+    SECURITY_PASSWORD_SALT = os.environ.get('SALT')
+    REMEMBER_COOKIE_SAMESITE = 'strict'
+    SESSION_COOKIE_SAMESITE = 'strict'
